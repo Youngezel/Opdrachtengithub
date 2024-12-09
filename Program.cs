db835d05e64c9ecd32c9fea04ec950cf922affb2
Sam <sam.smink023@gmail.com>
2024-12-09 15:00:38 +0100

    Dit is een mooi bericht

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testvoorgit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("je zus op een driewiller");
+        }
+    }
+}
